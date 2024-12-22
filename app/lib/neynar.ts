e49a2c693d3999ce64a9b8572778232b2afd97de
Quincybob <74205675+Quincybob@users.noreply.github.com>
2024-12-22 23:46:21 +0700

    neynar streamlined

--- app/lib/neynar.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@neynar/nodejs-sdk'
-
-if (!process.env.NEXT_PUBLIC_NEYNAR_API_KEY) {
-  throw new Error('Missing NEYNAR_API_KEY environment variable')
-}
-
-export const neynarClient = createClient(process.env.NEXT_PUBLIC_NEYNAR_API_KEY)
